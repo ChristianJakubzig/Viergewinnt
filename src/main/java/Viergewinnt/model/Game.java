25e27d128186182a8de2024fed973bb0755620cc
Jakubzig <wasserballer1@hotmail.de>
2024-08-24 19:55:53 +0200

    just some cleanup

--- src/main/java/Viergewinnt/model/Game.java
+++ src/main/java/Viergewinnt/model/Game.java
@@ -6,13 +6,11 @@ public class Game {
     private Boolean running;
     private Board board;
     private Inputhandler inputhandler;
-    private Player player;
 
     public Game() {
         this.running = true;
         this.board = new Board();
         this.inputhandler = new Inputhandler();
-        this.player = new Player();
     }
 
     public void gameRunning() {
