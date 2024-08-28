c01eb2c818bac74fcce146f8776f4561a32a9b44
Jakubzig <wasserballer1@hotmail.de>
2024-08-28 12:52:44 +0200

    exception handling implimented

--- src/main/java/Viergewinnt/controller/Inputhandler.java
+++ src/main/java/Viergewinnt/controller/Inputhandler.java
@@ -1,22 +1,37 @@
 package Viergewinnt.controller;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Inputhandler {
     private int inputColumn;
     Scanner scanner;
 
-    public Inputhandler(){
+    public Inputhandler() {
         this.scanner = new Scanner(System.in);
     }
 
-    public int userColumn(){
-        System.out.println("In welcher reihe möchtest du deinen Stein plazieren von 0 bis 7 bitte gib die entsprechende Zahl ein: ");
-        inputColumn = scanner.nextInt();
-        return inputColumn;
+    public int userColumn() {
+        while (true) {
+            System.out.println("In welcher reihe möchtest du deinen Stein plazieren von 0 bis 6 bitte gib die entsprechende Zahl ein: ");
+
+            try {
+                inputColumn = scanner.nextInt();
+
+                if (inputColumn < 0 || inputColumn > 6) {
+                    throw new IllegalArgumentException("Ungültige Eingabe! Bitte gib eine Valide Zahl zwischen 0 und 6 ein.");
+                }
+                return inputColumn;
+            } catch (InputMismatchException e) {
+                System.out.println("Ungültige Eingabe! Bitte gib eine Zahl ein.");
+                scanner.next();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public int getInputColumn(){
+    public int getInputColumn() {
         return this.inputColumn;
     }
 }
